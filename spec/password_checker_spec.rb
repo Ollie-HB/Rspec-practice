ea56f4d5ce189509f0050dd10a1088a14c95f960
Ollie Beck <ollieb94@hotmail.co.uk>
2022-09-14 12:49:00 +0100

    Error testing - Challenge One

--- spec/password_checker_spec.rb
+++ spec/password_checker_spec.rb
@@ -1,4 +1,13 @@
 require 'password_checker'
 
 RSpec.describe PasswordChecker do
-    
+  it "succeeds if password is greater than/equal to 8 characters" do
+    password = PasswordChecker.new
+    expect(password.check("Password")).to eq true
+  end
+
+  it "fails if password is less than 8 characters" do
+    password = PasswordChecker.new
+    expect { password.check("passwor")}.to raise_error "Invalid password, must be 8+ characters."
+  end
+end
